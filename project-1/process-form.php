b9932e1fa035c99795c42df5e051fdd093036ae7
dominicbaillie <dominicbaillie8@gmail.com>
2025-11-27 12:38:49 -0500

    Setup The Basic Website for Project 1

---
+++ project-1/process-form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Edit Payment Information - HammerCorp™</title>
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body>
+		<header>
+			<h1>HammerCorp™</h1>
+		</header>
+		<main>
+
+<h1>Your Purchase Information</h1>
+<body>
+    <main>
+
+        <?php
+            $Size = $_GET ["size"];
+            $Hammer = $_GET ["hammer"];
+            $Color = $_GET ["color"];
+            echo('<p>You have ordered a '.$Size.' '.$Color.' '.$Hammer.'.</p>');
+        ?>
+
+        <h2>Purchase Information</h2>
+        <?php
+            $fname = $_GET ["fname"];
+            
+            $lname = $_GET ["lname"];
+            
+            $alert = $_GET["alert"];
+            
+            echo('<p>Thank you, '.$fname.' '.$lname.'</p>');
+
+            echo('<p>The Alert Settings you have chosen include the following notifications:</p>
+            <ul>');
+
+            foreach ($alert as $item)
+            {
+                echo('<li>'.$item.'</li');
+            }
+
+
+            echo('</ul>');
+        ?>
+    </main>
+        <footer>
+            <p><small>©Hammer Corp. All rights reserved</small></p>
+        </footer>
+    </body>
+</html>
