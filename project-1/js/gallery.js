98af6c9bc06ed2a358ef5d422d8a798b0f715fb3
dominicbaillie <dominicbaillie8@gmail.com>
2025-12-09 17:21:28 -0500

    Fixed icon on sub pages

--- project-1/js/gallery.js
+++ project-1/js/gallery.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll("#gallery a")
 
 for (let i = 0; i < links.length; i++) 
     {
-    // STEP 4a: For the link that was clicked, capture the value of the href attribute as a variable called 'source'
+    
     links[i].addEventListener("click", function (event) 
     {
         let source = this.getAttribute("href");
